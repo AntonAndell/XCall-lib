68d2cc425a39fcf2e4778775c6d2a1f1ffe98891
AntonAndell <anton@andell.eu>
2023-05-29 07:39:11 +0200

    add getNetworkdId to xCall interface

--- score-lib/src/main/java/xcall/score/lib/interfaces/XCall.java
+++ score-lib/src/main/java/xcall/score/lib/interfaces/XCall.java
@@ -5,9 +5,14 @@ import java.math.BigInteger;
 import foundation.icon.score.client.ScoreInterface;
 import score.annotation.External;
 import score.annotation.Optional;
+import score.annotation.Payable;
 
 @ScoreInterface
 public interface XCall {
     @External
+    @Payable
     public BigInteger sendCallMessage(String _to, byte[] _data, @Optional byte[] _rollback);
+
+    @External(readonly = true)
+    public String getNetworkId();
 }
